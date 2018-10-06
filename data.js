3fd919747fb24f148e87b25723698ee17decd2bb
Wonzii <DonavanH@csedge.org>
2018-10-06 13:18:37 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,8 @@
 // Example
-let message = "Hello World!";
+let message = "Hello Donavan!";
 console.log(message);
 
 // Type your code below!
+let killsPerGame = 5
+let myHight = 5.4
+let shoeSize = 9
