65c6ce0d2e795a8eeda6d4ff12837660a1d1f6b1
Donavan Harris <DonavanH@csedge.org>
2018-10-13 15:50:53 -0700

    update

--- change.js
+++ change.js
@@ -1,26 +1,32 @@
 const READLINE = require('readline-sync');
 
-// let age = READLINE.question("how old am I: ");
-// console.log("I am " + age);
-//
-//
-// let userInput = READLINE.question("Type anything and I'll repeat it back!");
-// console.log(userInput);
+let age = READLINE.question("how old am I: ");
+console.log("I am " + age);
 
-// declare and assign a variable for pennies using readlineSync
-// ask the user how many pennies he/she wants change for
 
-let peenies = READLINE.quesion('please enter cents as a positive integer ');
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+console.log(userInput);
+
+declare and assign a variable for pennies using readlineSync
+ask the user how many pennies he/she wants change for
+
+let pennies = READLINE.quesion('please enter cents as a positive integer ');
 
 
 // declare, assign, and print how many quarters you can make out of the pennies
 
 let quarters =Math.floor(pennies/25);
 
+let remainder = pennies %25;
+
+let dimes = Math.floor(remainder/10);
+
 console.log(quarters, 'QUARTERS');
 
-// declare, assign, and print how many dimes you can make out of the pennies
+console.log(dimes, 'DIMES');
 
+
+// declare, assign, and print how many dimes you can make out of the pennies
 
 
 
